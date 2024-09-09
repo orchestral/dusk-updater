d49f890dd1c2ed8795280822d6dc57b43e9107d5
Mior Muhammad Zaki <crynobone@gmail.com>
2024-09-09 18:09:06 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Concerns/DetectsChromeVersion.php
+++ src/Concerns/DetectsChromeVersion.php
@@ -137,7 +137,6 @@ trait DetectsChromeVersion
             }
 
             /** @var array{0: string, 1: string, 2: string, 3: string} $matches */
-
             $semver = implode('.', [$matches[1], $matches[2], $matches[3]]);
 
             return [
@@ -181,7 +180,6 @@ trait DetectsChromeVersion
 
             if (isset($matches[1])) {
                 /** @var array{0: string, 1: string, 2: string, 3: string} $matches */
-
                 $semver = implode('.', [$matches[1], $matches[2], $matches[3]]);
 
                 return [
